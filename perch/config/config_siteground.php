f075858deffd20098b1d51b4e9a50979cda1c03c
Nicholas Fazzolari <pdxpunk85@gmail.com>
2020-03-22 19:57:42 -0700

    Added siteground connection in config_siteground.php

---
+++ perch/config/config_siteground.php
@@ -0,0 +1,22 @@
+<?php
+    define('PERCH_LICENSE_KEY', 'P32001-ACV753-QGW480-YMU177-KMD019');
+
+    define("PERCH_DB_USERNAME", 'Blackwa5_barusr');
+    define("PERCH_DB_PASSWORD", 'E}2A@Cl}TzTu');
+    define("PERCH_DB_SERVER", "localhost");
+    define("PERCH_DB_DATABASE", "blackwa5_bar");
+    define("PERCH_DB_PREFIX", "perch3_");
+    
+    define('PERCH_TZ', 'UTC');
+
+    define('PERCH_EMAIL_FROM', 'btsourcepdx@gmail.com');
+    define('PERCH_EMAIL_FROM_NAME', 'Nicholas Fazzolari');
+
+    define('PERCH_LOGINPATH', '/black_water_bar/perch');
+    define('PERCH_PATH', str_replace(DIRECTORY_SEPARATOR.'config', '', __DIR__));
+    define('PERCH_CORE', PERCH_PATH.DIRECTORY_SEPARATOR.'core');
+
+    define('PERCH_RESFILEPATH', PERCH_PATH . DIRECTORY_SEPARATOR . 'resources');
+    define('PERCH_RESPATH', PERCH_LOGINPATH . '/resources');
+    
+    define('PERCH_HTML5', true);
